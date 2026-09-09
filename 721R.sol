a144410f7291b8895e2d1a11aed8cd6b0c369398
justv030-cyber <justv030@gmail.com>
2026-09-09 15:36:35 +0530

    add erc721A and ierc721R and abstarct

--- 721R.sol
+++ 721R.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.27;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/exo-digital-labs/ERC721R/blob/main/contracts/ERC721A.sol";
+import "https://github.com/exo-digital-labs/ERC721R/blob/main/contracts/IERC721R.sol";
 
-contract Cortex is ERC721, Ownable {
-    constructor(address initialOwner)
-        ERC721("Cortex ", "CTX")
-        Ownable(initialOwner)
-    {}
+abstract contract Cortex is ERC721A, IERC721R, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC721A("Cortex ", "CTX") Ownable(initialOwner) {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmbseRTJWSsLfhsiWwuB2R7EtN93TxfoaMz1S5FXtsFEUB/";
