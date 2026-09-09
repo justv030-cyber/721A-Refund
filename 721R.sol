634aad60411f21ebcbf64592b06ca3590da5941e
justv030-cyber <justv030@gmail.com>
2026-09-09 17:36:35 +0530

    total refund functionality working and tested

--- 721R.sol
+++ 721R.sol
@@ -12,9 +12,22 @@ abstract contract Cortex is ERC721A, IERC721R, Ownable {
     uint256 public constant mintPerUser = 4;
     uint256 public constant totalSupplyLimit = 10000;
 
+    uint256 public constant refundPeriod = 10 days;
+
+    mapping(uint256 => uint256) public refundTimestamps;
+
+    mapping(uint256 => bool) public hasRefunded;
+
+    address public refundAdddress;
+    uint256 public refundTimeStamp;
+    uint256 public refundAmt;
+
     constructor(
         address initialOwner
-    ) ERC721A("Cortex ", "CTX") Ownable(initialOwner) {}
+    ) ERC721A("Cortex ", "CTX") Ownable(initialOwner) {
+        refundAdddress = address(this);
+        refundTimeStamp = block.timestamp + refundPeriod;
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "ipfs://QmbseRTJWSsLfhsiWwuB2R7EtN93TxfoaMz1S5FXtsFEUB/";
@@ -33,6 +46,52 @@ abstract contract Cortex is ERC721A, IERC721R, Ownable {
         );
 
         _safeMint(msg.sender, quantity);
+
+        refundTimeStamp = block.timestamp + refundPeriod;
+
+        for (uint256 i = _currentIndex - quantity; i < _currentIndex; i++) {
+            refundTimestamps[i] = refundTimeStamp;
+        }
+    }
+
+    function refund(uint256 tokenId) external {
+        require(
+            msg.sender == ownerOf(tokenId),
+            "You Are Not Owner Of This NFT"
+        );
+        require(
+            block.timestamp <= getrefundDeadLine(tokenId),
+            "Deadline Reached"
+        );
+        require(!hasRefunded[tokenId], "Already Refunded");
+        refundAmt = getRefundAmt(tokenId);
+
+        //ownership
+        _transfer(address(this), refundAdddress, tokenId);
+
+        // pay
+
+        (bool sucess, ) = payable(msg.sender).call{value: refundAmt}("");
+
+        require(sucess, "Transfer Failed");
+
+        // make a yes refuned NFT
+
+        hasRefunded[tokenId] = true;
+    }
+
+    function getrefundDeadLine(uint256 tokenId) public view returns (uint256) {
+        if (hasRefunded[tokenId]) {
+            return 0;
+        }
+        return refundTimestamps[tokenId];
+    }
+
+    function getRefundAmt(uint256 tokenId) public view returns (uint256) {
+        if (hasRefunded[tokenId]) {
+            return 0;
+        }
+        return mintPrice;
     }
 
     function withdraw() external onlyOwner {
