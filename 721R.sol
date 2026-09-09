a06a0e8682dad1b88a1be420993b712a2ea4c0fc
justv030-cyber <justv030@gmail.com>
2026-09-09 15:30:53 +0530

    update cortex token with erc721A

--- 721R.sol
+++ 721R.sol
@@ -1,1 +1,21 @@
-// hello inital commit
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.7.0
+pragma solidity ^0.8.27;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Cortex is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("Cortex ", "CTX")
+        Ownable(initialOwner)
+    {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmbseRTJWSsLfhsiWwuB2R7EtN93TxfoaMz1S5FXtsFEUB/";
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
